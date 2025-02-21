632cf71721954b2076d88dd040e3bb18b5f668db
aleja <aleja@SANTINI>
2025-02-20 22:10:08 -0300

    Add project files.

---
+++ SistemaGestionExamenes/FormPreguntas.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace SistemaGestionExamenes
+{
+    partial class FormPreguntas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ingresarPregunta = new System.Windows.Forms.Label();
+            this.textBoxIngresarPregunta = new System.Windows.Forms.TextBox();
+            this.txtRespuesta1 = new System.Windows.Forms.Label();
+            this.txtRespuesta2 = new System.Windows.Forms.Label();
+            this.txtRespuesta3 = new System.Windows.Forms.Label();
+            this.txtRespuesta4 = new System.Windows.Forms.Label();
+            this.textBoxOpcion1 = new System.Windows.Forms.TextBox();
+            this.textBoxOpcion2 = new System.Windows.Forms.TextBox();
+            this.textBoxOpcion3 = new System.Windows.Forms.TextBox();
+            this.textBoxOpcion4 = new System.Windows.Forms.TextBox();
+            this.comboBoxRespuestaCorrecta = new System.Windows.Forms.ComboBox();
+            this.comboBoxAsignatura = new System.Windows.Forms.ComboBox();
+            this.labelSeleccionarRespuesta = new System.Windows.Forms.Label();
+            this.labelSeleccioneAsignatura = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ingresarPregunta
+            // 
+            this.ingresarPregunta.AutoSize = true;
+            this.ingresarPregunta.Location = new System.Drawing.Point(22, 31);
+            this.ingresarPregunta.Name = "ingresarPregunta";
+            this.ingresarPregunta.Size = new System.Drawing.Size(123, 13);
+            this.ingresarPregunta.TabIndex = 0;
+            this.ingresarPregunta.Text = "Ingresar nueva pregunta";
+            // 
+            // textBoxIngresarPregunta
+            // 
+            this.textBoxIngresarPregunta.Location = new System.Drawing.Point(25, 47);
+            this.textBoxIngresarPregunta.Name = "textBoxIngresarPregunta";
+            this.textBoxIngresarPregunta.Size = new System.Drawing.Size(120, 20);
+            this.textBoxIngresarPregunta.TabIndex = 1;
+            // 
+            // txtRespuesta1
+            // 
+            this.txtRespuesta1.AutoSize = true;
+            this.txtRespuesta1.Location = new System.Drawing.Point(22, 90);
+            this.txtRespuesta1.Name = "txtRespuesta1";
+            this.txtRespuesta1.Size = new System.Drawing.Size(53, 13);
+            this.txtRespuesta1.TabIndex = 2;
+            this.txtRespuesta1.Text = "Opcion 1:";
+            // 
+            // txtRespuesta2
+            // 
+            this.txtRespuesta2.AutoSize = true;
+            this.txtRespuesta2.Location = new System.Drawing.Point(143, 90);
+            this.txtRespuesta2.Name = "txtRespuesta2";
+            this.txtRespuesta2.Size = new System.Drawing.Size(53, 13);
+            this.txtRespuesta2.TabIndex = 3;
+            this.txtRespuesta2.Text = "Opcion 2:";
+            // 
+            // txtRespuesta3
+            // 
+            this.txtRespuesta3.AutoSize = true;
+            this.txtRespuesta3.Location = new System.Drawing.Point(269, 90);
+            this.txtRespuesta3.Name = "txtRespuesta3";
+            this.txtRespuesta3.Size = new System.Drawing.Size(53, 13);
+            this.txtRespuesta3.TabIndex = 4;
+            this.txtRespuesta3.Text = "Opcion 3:";
+            // 
+            // txtRespuesta4
+            // 
+            this.txtRespuesta4.AutoSize = true;
+            this.txtRespuesta4.Location = new System.Drawing.Point(396, 90);
+            this.txtRespuesta4.Name = "txtRespuesta4";
+            this.txtRespuesta4.Size = new System.Drawing.Size(53, 13);
+            this.txtRespuesta4.TabIndex = 5;
+            this.txtRespuesta4.Text = "Opcion 4:";
+            // 
+            // textBoxOpcion1
+            // 
+            this.textBoxOpcion1.Location = new System.Drawing.Point(25, 106);
+            this.textBoxOpcion1.Name = "textBoxOpcion1";
+            this.textBoxOpcion1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOpcion1.TabIndex = 6;
+            // 
+            // textBoxOpcion2
+            // 
+            this.textBoxOpcion2.Location = new System.Drawing.Point(146, 106);
+            this.textBoxOpcion2.Name = "textBoxOpcion2";
+            this.textBoxOpcion2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOpcion2.TabIndex = 7;
+            // 
+            // textBoxOpcion3
+            // 
+            this.textBoxOpcion3.Location = new System.Drawing.Point(272, 106);
+            this.textBoxOpcion3.Name = "textBoxOpcion3";
+            this.textBoxOpcion3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOpcion3.TabIndex = 8;
+            // 
+            // textBoxOpcion4
+            // 
+            this.textBoxOpcion4.Location = new System.Drawing.Point(399, 106);
+            this.textBoxOpcion4.Name = "textBoxOpcion4";
+            this.textBoxOpcion4.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOpcion4.TabIndex = 9;
+            // 
+            // comboBoxRespuestaCorrecta
+            // 
+            this.comboBoxRespuestaCorrecta.FormattingEnabled = true;
+            this.comboBoxRespuestaCorrecta.Location = new System.Drawing.Point(25, 173);
+            this.comboBoxRespuestaCorrecta.Name = "comboBoxRespuestaCorrecta";
+            this.comboBoxRespuestaCorrecta.Size = new System.Drawing.Size(162, 21);
+            this.comboBoxRespuestaCorrecta.TabIndex = 10;
+            // 
+            // comboBoxAsignatura
+            // 
+            this.comboBoxAsignatura.FormattingEnabled = true;
+            this.comboBoxAsignatura.Location = new System.Drawing.Point(221, 173);
+            this.comboBoxAsignatura.Name = "comboBoxAsignatura";
+            this.comboBoxAsignatura.Size = new System.Drawing.Size(162, 21);
+            this.comboBoxAsignatura.TabIndex = 11;
+            // 
+            // labelSeleccionarRespuesta
+            // 
+            this.labelSeleccionarRespuesta.AutoSize = true;
+            this.labelSeleccionarRespuesta.Location = new System.Drawing.Point(22, 157);
+            this.labelSeleccionarRespuesta.Name = "labelSeleccionarRespuesta";
+            this.labelSeleccionarRespuesta.Size = new System.Drawing.Size(165, 13);
+            this.labelSeleccionarRespuesta.TabIndex = 12;
+            this.labelSeleccionarRespuesta.Text = "Seleccione la respuesta correcta:";
+            // 
+            // labelSeleccioneAsignatura
+            // 
+            this.labelSeleccioneAsignatura.AutoSize = true;
+            this.labelSeleccioneAsignatura.Location = new System.Drawing.Point(218, 157);
+            this.labelSeleccioneAsignatura.Name = "labelSeleccioneAsignatura";
+            this.labelSeleccioneAsignatura.Size = new System.Drawing.Size(126, 13);
+            this.labelSeleccioneAsignatura.TabIndex = 13;
+            this.labelSeleccioneAsignatura.Text = "Seleccione la asignatura:";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(25, 364);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(121, 23);
+            this.btnGuardar.TabIndex = 14;
+            this.btnGuardar.Text = "Guardar Pregunta";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // FormPreguntas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.labelSeleccioneAsignatura);
+            this.Controls.Add(this.labelSeleccionarRespuesta);
+            this.Controls.Add(this.comboBoxAsignatura);
+            this.Controls.Add(this.comboBoxRespuestaCorrecta);
+            this.Controls.Add(this.textBoxOpcion4);
+            this.Controls.Add(this.textBoxOpcion3);
+            this.Controls.Add(this.textBoxOpcion2);
+            this.Controls.Add(this.textBoxOpcion1);
+            this.Controls.Add(this.txtRespuesta4);
+            this.Controls.Add(this.txtRespuesta3);
+            this.Controls.Add(this.txtRespuesta2);
+            this.Controls.Add(this.txtRespuesta1);
+            this.Controls.Add(this.textBoxIngresarPregunta);
+            this.Controls.Add(this.ingresarPregunta);
+            this.Name = "FormPreguntas";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ingresarPregunta;
+        private System.Windows.Forms.TextBox textBoxIngresarPregunta;
+        private System.Windows.Forms.Label txtRespuesta1;
+        private System.Windows.Forms.Label txtRespuesta2;
+        private System.Windows.Forms.Label txtRespuesta3;
+        private System.Windows.Forms.Label txtRespuesta4;
+        private System.Windows.Forms.TextBox textBoxOpcion1;
+        private System.Windows.Forms.TextBox textBoxOpcion2;
+        private System.Windows.Forms.TextBox textBoxOpcion3;
+        private System.Windows.Forms.TextBox textBoxOpcion4;
+        private System.Windows.Forms.ComboBox comboBoxRespuestaCorrecta;
+        private System.Windows.Forms.ComboBox comboBoxAsignatura;
+        private System.Windows.Forms.Label labelSeleccionarRespuesta;
+        private System.Windows.Forms.Label labelSeleccioneAsignatura;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ SistemaGestionExamenes/FormPreguntas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaGestionExamenes
+{
+    public partial class FormPreguntas: Form
+    {
+        public FormPreguntas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string preguntaId = Guid.NewGuid().ToString();
+            string pregunta = textBoxIngresarPregunta.Text;
+            string respuestas = $"{txtRespuesta1.Text}|{txtRespuesta2.Text}|{txtRespuesta3.Text}|{txtRespuesta4.Text}";
+            string correcta = comboBoxRespuestaCorrecta.SelectedItem.ToString();
+            string asignatura = comboBoxAsignatura.SelectedItem.ToString();
+
+            string linea = $"{preguntaId},{pregunta},{respuestas},{correcta},{asignatura}";
+
+            File.AppendAllText("Preguntas.txt", linea + Environment.NewLine);
+            MessageBox.Show("Pregunta guardada con éxito");
+        }
+    }
+}
